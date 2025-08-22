67130ce0aaec61862c5961482d14b4ea2a0b655c
Szondy, Katalin (TNG) <katalin.szondy@tngtech.com>
2025-08-22 10:58:16 +0200

    Update personal info

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
-    // In the following fiels you can either give a single string,
+    name: "Kata Szondy",
+    // In the following fields you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Automated testing and deployment"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Jenkins",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Get a better understanding of Jenkins", "Learn how to setup a CI/CD pipeline"],
   },
 });
